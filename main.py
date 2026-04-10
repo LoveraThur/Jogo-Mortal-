d4c7670d6aa810d02f82c25da84fe1851037ea08
ArthurSilvani <arthurpsilvani@gmail.com>
2026-04-10 01:00:17 -0300

    Fix: Fase4

--- main.py
+++ main.py
@@ -100,10 +100,49 @@ time.sleep(5)
 os.system('clear')
 print('Responda a seguinte Fórmula:\n   (respostaFase1 + respostaFase2)**respostaFase1 - respostaFase2')
 operacao2 = (resposta1 + resposta2) ** resposta1 - resposta2
+print(operacao2)
 resposta3 = int(input('Resposta da Fórmula:\n >>>'))
 if resposta3 == operacao2:
     print('\033[32mResposta Correta! \033[31m')
     time.sleep(3)
 else:
     os.system('clear')
-    print(deadMessage)   
+    print(deadMessage)
+
+# ---------------------------------------------
+#FASE 4
+
+os.system('clear')
+
+sort3 = random.randint(1,101)
+sort4 = random.randint(1,101)
+sort5 = random.randint(1,101)
+sort6 = random.randint(1,101)
+sort7 = random.randint(1,101)
+list = (sort3, sort4, sort5, sort6, sort7)
+
+time.sleep(2)
+os.system('clear')
+print(list)
+contador = 0
+while True:
+    contador += 1 
+    resposta4 = int(input(f'Qual foi o {contador}° número?\n>>> '))
+    
+    if resposta4 == sort3:
+        pass
+    elif resposta4 == sort4:
+        pass
+    elif resposta4 == sort5:
+        pass
+    elif resposta4 == sort6:
+        pass
+    elif resposta4 == sort7:
+        print('VOcê acertou todos os números')
+        break
+    else:
+        print(deadMessage)
+        break
+
+# ---------------------------------------------
+#FASE 5
